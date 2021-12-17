9346b1ce278176c00addf4ae0a68094f39acc391
Harry <Harry.Schneider@AquilaHeywood.co.uk>
2021-12-17 16:10:02 +0000

    day 9 part 1

---
+++ src/test/java/day9/SmokeBasinTest.groovy
@@ -0,0 +1,15 @@
+package day9
+
+import spock.lang.Specification
+
+class SmokeBasinTest extends Specification {
+
+    def "Sum risk levels og a given map file"() {
+        given: "A map file"
+            File testFile = new File(getClass().getResource('/day9/testInput.txt').toURI())
+        when: "We calculate the sum the risk levels"
+            int answer = SmokeBasin.sumRiskLevels(testFile)
+        then: "We get the right answer"
+            answer == 15
+    }
+}
